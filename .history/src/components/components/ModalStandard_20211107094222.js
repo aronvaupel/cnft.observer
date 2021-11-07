88d4f0ba75a9272ea9fb50413f4308b5ac4c2056
Aron Vaupel <vaupelaron@gmail.com>
2021-11-07 12:49:00 +0100

    create modal correct selection for handleCLick

--- .history/src/components/components/ModalStandard_20211107094222.js
+++ .history/src/components/components/ModalStandard_20211107094222.js
@@ -3,17 +3,9 @@ import {Modal, Button} from 'react-bootstrap';
 
 
 const ModalStandard = ()  => {
-  const [show, setShow] = useState(false);
-
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
